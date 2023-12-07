eaebccb2da8203515e15314552506950960eb98c
Bethuel Heldt <bethuel@heldt.nl>
2023-12-07 12:52:01 +0100

    nav js gemaakt om bij scroll nav selected te zetten

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ for (var x = 0; x < nav__link.length; ++x) {
         console.log("input sibling is checked");
     })
 }
+
+// bij scrollen nav selected zetten
+const sections = document.querySelectorAll("article");
+const navLink = document.querySelectorAll("nav label a");
+window.onscroll = () => {
+    var current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 100) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLink.forEach((a) => {
+        a.previousElementSibling.checked = false;
+        //console.log('a: '+a+' - current: '+current+' href: '+a.getAttribute("href"));
+        if (a.getAttribute("href") == "#"+current) {
+            a.previousElementSibling.checked = true;
+        }
+    });
+};
